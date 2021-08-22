8e13af8057f708c32ca34e61b20ddcf3340f13a6
CONDE Ansou <70788397+ansouconde93@users.noreply.github.com>
2021-08-23 01:58:29 +0200

    Update eComm.service.ts

--- src/app/service/eComm.service.ts
+++ src/app/service/eComm.service.ts
@@ -24,6 +24,7 @@ export class ECommService {
     return this.http.post<any>(this.host+url, ressources,{
       headers: new HttpHeaders(
         {
+          'Access-Control-Allow-Origin': '*',
           'Authorization':token
         }
       )
